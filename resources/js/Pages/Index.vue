d8220f9cd14fe0d591c22df73ae0a8271bb90cf9
RuslanPepe <yypaau@gmail.com>
2026-04-13 20:29:35 +0300

    add Carousel in process

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,14 +2,26 @@
     <div class="upload-area" @click="$refs.inputFile.click()">
         Нажми или перетащи фото 📷
     </div>
-    <input id="formFileMultiple" ref="inputFile" type="file" accept="image/*" @input="fileStore.fileHandleInput($refs.inputFile.files)" multiple hidden>
-    <div class="carouserContainer">
-        <div class="preview" id="preview">
-            <button class="" id="scrollLeft">‹</button>
-            <img v-for="file in fileStore.files" :src="file" alt="err...">
-            <button class="" id="scrollRight">›</button>
+    <input id="formFileMultiple" ref="inputFile" type="file" accept="image/*" @input="fileStore.fileHandleInput($refs.inputFile)" multiple hidden>
+    <div class="carouserContainer flex-column" v-if="fileStore.files.length">
+        <div class="row">
+            <div class="col-1 d-flex">
+                <button class="arrowButton ms-auto" id="scrollLeft">‹</button>
+            </div>
+            <div class="col-10">
+                <div class="preview position-relative align-items-center"  id="preview">
+                    <div class="imageBlock position-relative" v-for="(file, index) in fileStore.files" :key="index">
+                        <button type="button" class="btnRemoveImage align-items-center justify-content-center">x</button>
+                        <img height="280" :src="file" alt="err...">
+                    </div>
+                </div>
+            </div>
+            <div class="col-1">
+                <button class="arrowButton" id="scrollRight">›</button>
+            </div>
         </div>
     </div>
+<!--    {{ fileStore.files.length }}-->
 
 
 
@@ -28,8 +40,34 @@ const fileStore = useFileStore()
 </script>
 
 <style scoped>
+.btnRemoveImage {
+    position: absolute;
+    background: rgba(255,255,255,.03);
+    border-radius: 5px;
+    border: none;
+    margin: 0;
+    padding: 0;
+    color: white;
+    right: -10px;
+    top: -10px;
+    font-size: 16px;
+    z-index: 3;
+    width: 24px;
+    height: 24px;
+}
 .preview {
     height: 300px;
+}
+
+.arrowButton {
+    background: none;
+    border: none;
+    color: white;
+    font-size: 32px;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 2;
 }
 
 </style>
--- resources/js/stores/file.js
+++ resources/js/stores/file.js
@@ -5,7 +5,15 @@ import router from "../Router.js";
 export const useFileStore = defineStore('file', {
     state: () => {
         return {
-            files: [],
+            // files: [],
+            files: [
+                "/screenshot-2026-03-25-17:10:31.png",
+                "/screenshot-2026-03-25-17:10:31.png",
+                "/screenshot-2026-03-25-17:10:31.png",
+                "/screenshot-2026-03-25-17:10:31.png",
+                "/screenshot-2026-03-25-17:10:31.png",
+            ],
+
         }
     },
 
@@ -13,10 +21,12 @@ export const useFileStore = defineStore('file', {
     actions: {
         fileHandleInput(el) {
             console.log(el)
-            Array.from(el).forEach((file) => {
+            Array.from(el.files).forEach((file) => {
                 this.files.push(URL.createObjectURL(file))
                 console.log(URL.createObjectURL(file))
             })
+            el.value = null
+            console.log(this.files)
             // for (const file in el.item()) {
             //     console.log(file)
             // }
